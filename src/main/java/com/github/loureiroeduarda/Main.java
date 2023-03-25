a0fac0c61e0bee1a14c46a3f135dd8e07307c639
loureiroeduarda <105324693+loureiroeduarda@users.noreply.github.com>
2023-03-25 13:14:29 -0300

    Read csv file (#1)
    * Project removed
    
    * Created project with maven
    
    * Implemented csv file read

---
+++ src/main/java/com/github/loureiroeduarda/Main.java
@@ -0,0 +1,11 @@
+package com.github.loureiroeduarda;
+
+import com.github.loureiroeduarda.csv.ReadCsv;
+
+public class Main {
+    public static void main(String[] args) {
+        ReadCsv readCsv = new ReadCsv();
+
+        readCsv.read();
+    }
+}
---
+++ src/main/java/com/github/loureiroeduarda/csv/ReadCsv.java
@@ -0,0 +1,35 @@
+package com.github.loureiroeduarda.csv;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReadCsv {
+    public void read() {
+
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/DNIT-Distancias.csv"));
+            CSVReader csvReader = new CSVReaderBuilder(reader).build();
+
+            List<String[]> distanceCities = csvReader.readAll();
+            for (String[] line : distanceCities)
+                System.out.println(Arrays.asList(line));
+
+        } catch (NoSuchFileException noSuchFileException) {
+            System.out.println("Arquivo não encontrado.");
+        } catch (IOException ioException) {
+            System.out.println("Leitura interrompida");
+        } catch (CsvException csvException) {
+            System.out.println("Arquivo corrompido");
+        }
+    }
+
+}
